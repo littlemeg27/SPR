6814609ae42addd33030ce711506f71a389dc3a5
Brenna Pavlinchak <raven.mobile.design@gmail.com>
2022-09-03 14:54:51 -0400

    Initial commit

---
+++ 1-1/1-1/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  1-1
+//
+//  Created by Brenna Pavlinchak on 9/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    char initials = 'value';
+    int age;
+
+    return 0;
+}
