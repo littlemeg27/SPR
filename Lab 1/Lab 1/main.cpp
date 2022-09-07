0fc24d015f4c46f31fdd29b9dd401711fd7801c7
Brenna Pavlinchak <raven.mobile.design@gmail.com>
2022-09-06 20:41:20 -0400

    Fixed up to 1-5

--- Lab 1/Lab 1/main.cpp
+++ Lab 1/Lab 1/main.cpp
@@ -40,74 +40,61 @@ int main(int argc, const char * argv[])
 //Calls Program 1-1
 void ArrayOfChars()
 {
-    int i = 0;
-    int j = 0;
+    int newUserAge;
+    int userAge;
+    char userInitials[5];
+
+    cout << "Please enter your Initials \n";
+    cin >> userInitials;
+    cout << "Please enter your Age \n";
+    cin >> userAge;
+
     
-    for (int k = 0; k < 2; k++)
+    if(!cin)
     {
-        int userAge [j];
-        char userInitials[i];
-    
-            for (i = 0; i < 1; i++)
-            {
-                cout << "Please enter your Initials \n";
-                cin >> userInitials[i];
-                cin.ignore(1000);
-            }
-            if(cin.fail())
-            {
-                cin.clear();
-            }
-                
-            if(!cin.fail())
-            {
-                break;
-            }
-            for (j = 0; j < 1; j++)
-            {
-                cout << "Please enter your Age \n";
-                cin >> userAge[j];
-                cin.ignore(1000);
-            }
-        
-        int newUserAge =  userAge[j] * 365;
-        cout << userInitials[i] << "did know you might be least " << newUserAge << "days old??";
+        cout << "That was no integer! Please enter an integer: ";
+        cin.clear();
+        cin >> userAge;
     }
+
+    newUserAge = userAge * 365;
+    cout << userInitials << " did know you might be least " << newUserAge << " days old??";
 }
 
 //Calls Program 1-2
 void ArrayOfInts()
 {
     int i;
-    int randomNumbers[4];
+    int randomNumbers[5];
 
-        for (i = 0; i <= 4; i++)
-        {
-            cout << "Please enter a Random Number \n";
-            cin >> randomNumbers[i];
-            cin.ignore(1000);
-        }
-    cout << randomNumbers[i] << "Your Random Numbers are: ";
+    for (i = 0; i <= 4; i++)
+    {
+        cout << "\nPlease enter a Random Number ";
+        cin >> randomNumbers[i];
+    }
+
+        cout << "Your Random Numbers are: " << randomNumbers[0] << ", " << randomNumbers[1] << ", " << randomNumbers[2] << ", " << randomNumbers[3] << ", " << randomNumbers[4];
+
+    
 }
 
 //Calls Program 1-3
 void Equations()
 {
     int i;
-    int equationArray[3];
+    int equationArray[4];
     int equationOne;
     int equationTwo;
 
-        for (i = 0; i <= 3; i++)
+        for (i = 0; i < 3; i++)
         {
-            cout << "Please enter a Random Number \n";
+            cout << "\nPlease 3 different numbers for the Equation! \n";
             cin >> equationArray[i];
-            cin.ignore(1000);
         }
     equationOne = equationArray[0] + 1 * equationArray[1] + 2 - equationArray[2];
     equationTwo = (equationArray[0] + 1) * (equationArray[1] + 2) - equationArray[2];
     
-    cout << "The anwser to Equation 1 is: " << equationOne;
+    cout << "The anwser to Equation 1 is: " << equationOne << " \n";
     cout << "The anwser to Equation 2 is: " << equationTwo;
 }
 
@@ -121,25 +108,16 @@ void Ranges()
 void ArrayOfCharName()
 {
     char nameArray[32];
-    string randomQuotes[4] = {"In three words I can sum up everything I've learned about life: it goes on.",
-                              "To live is the rarest thing in the world. Most people exist, that is all.",
-                              "Good friends, good books, and a sleepy conscience: this is the ideal life.",
-                              "Sometimes the questions are complicated and the answers are simple."};
+    string randomQuotes[4] = {" In three words I can sum up everything I've learned about life: it goes on.",
+                              " To live is the rarest thing in the world. Most people exist, that is all.",
+                              " Good friends, good books, and a sleepy conscience: this is the ideal life.",
+                              " Sometimes the questions are complicated and the answers are simple."};
     
-        for (int i = 0; i < 1; i++)
-        {
-            cout << "Please enter your full name \n";
-            cin.getline(nameArray,32);
-            cin.ignore(1000);
-        }
-    
-        for (int j = 0; j < 32; j++)
-        {
-            for (int k = 0; k < 4; k++)
-            {
-                cout << nameArray[j] << randomQuotes[k];
-            }
-        }
+    for (int i = 0; i < 1; i++)
+    {
+        cout << "\n\n\nPlease enter your full name \n";
+        cin.getline(nameArray,) << ;
+    }
 }
 
 //Calls Program 1-6
@@ -149,17 +127,17 @@ void AgeCheck()
     int ageCheck2 = 21;
     int randomAge = 0;
     
-    cout << "Enter a random age!";
+    cout << " Enter a random age!";
     cin >> randomAge;
     cin.ignore(1000);
 
     if(ageCheck1 >= randomAge)
     {
-        cout << "Mom said your not allowed!";
+        cout << " Mom said your not allowed!";
     }
     else if (ageCheck2 <= randomAge)
     {
-        cout << "It's Ok. You can go!";
+        cout << " It's Ok. You can go!";
     }
 }
 
