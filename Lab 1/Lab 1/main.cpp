ee6b6572ee2af509aed3b3f4ca057f08b0d892de
Brenna Pavlinchak <raven.mobile.design@gmail.com>
2022-09-06 22:58:00 -0400

    Did all but not 1-4

--- Lab 1/Lab 1/main.cpp
+++ Lab 1/Lab 1/main.cpp
@@ -95,29 +95,33 @@ void Equations()
     equationTwo = (equationArray[0] + 1) * (equationArray[1] + 2) - equationArray[2];
     
     cout << "The anwser to Equation 1 is: " << equationOne << " \n";
-    cout << "The anwser to Equation 2 is: " << equationTwo;
+    cout << "The anwser to Equation 2 is: " << equationTwo << "\n";
 }
 
 //Calls Program 1-4
 void Ranges()
 {
-    
+
+
 }
 
 //Calls Program 1-5
 void ArrayOfCharName()
 {
     char nameArray[32];
-    string randomQuotes[4] = {" In three words I can sum up everything I've learned about life: it goes on.",
+    string randomQuotes[5] = {" In three words I can sum up everything I've learned about life: it goes on.",
                               " To live is the rarest thing in the world. Most people exist, that is all.",
                               " Good friends, good books, and a sleepy conscience: this is the ideal life.",
                               " Sometimes the questions are complicated and the answers are simple."};
-    
+
+        cout << "\n\n\nPlease enter your full name \n";
+        cin.get(nameArray,32);
+
     for (int i = 0; i < 1; i++)
     {
-        cout << "\n\n\nPlease enter your full name \n";
-        cin.getline(nameArray,) << ;
+        cout << nameArray << ", "<< randomQuotes[i];
     }
+    
 }
 
 //Calls Program 1-6
@@ -127,9 +131,8 @@ void AgeCheck()
     int ageCheck2 = 21;
     int randomAge = 0;
     
-    cout << " Enter a random age!";
+    cout << "\n\nEnter a random age!\n";
     cin >> randomAge;
-    cin.ignore(1000);
 
     if(ageCheck1 >= randomAge)
     {
@@ -139,28 +142,101 @@ void AgeCheck()
     {
         cout << " It's Ok. You can go!";
     }
+    else
+    {
+        cout << " The more the years go by, the more difficult it gets. I'm getting old.";
+    }
 }
 
 //Calls Program 1-7
 void VerifyEvenNumber()
 {
+    int evenNumber;
+    cout << "\n\nEnter a random number!\n";
+    cin >> evenNumber;
     
+    if (evenNumber % 2 == 0)
+    {
+        cout << "First number " << evenNumber << " is divisible by second number " << 2;
+    }
+    else
+    {
+        cout << "First number " << evenNumber << " is not divisible by second number " << 2;
+    }
 }
 
 //Calls Program 1-8
 void EvenlyDivide()
 {
+    int number;
     
+    cout << "\n\nEnter a number to see if it is divisible!\n";
+    cin >> number;
+
+    for(int i = 1; i <= number; ++i)
+    {
+        if(number %i ==0)
+        {
+            cout << " " << i;
+        }
+    }
 }
 
 //Calls Program 1-9
 void PopsiscleColor()
 {
+    int colorChoice;
     
+    cout << "\n\nEnter a number from 1-8 to pick a Popsicle Color!\n";
+    cin >> colorChoice;
+    
+    switch (colorChoice)
+    {
+        case 1:
+            cout << "Violet";
+        break;
+        case 2:
+            cout << "Green";
+        break;
+        case 3:
+            cout << "Pink";
+        break;
+        case 4:
+            cout << "Yellow";
+        break;
+        case 5:
+            cout << "Blue";
+        break;
+        case 6:
+            cout << "Orange";
+        break;
+        case 7:
+            cout << "Red";
+        break;
+        case 8:
+            cout << "Purple";
+        break;
+    }
 }
 
 //Calls Program 1-10
 void GameLevels()
 {
+    int gameLevel;
     
+    cout << "\n\nEnter a number from 1-3 to pick a Game Level!\n";
+    cin >> gameLevel;
+    
+    switch (gameLevel)
+    {
+        case 1:
+            cout << "3";
+        break;
+        case 2:
+            cout << "11";
+        break;
+        case 3:
+            cout << "25";
+        break;
+    }
 }
