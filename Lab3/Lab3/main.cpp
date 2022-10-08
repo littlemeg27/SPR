e9114c7bf1fa9a69b0f4ee8dd84a4be3efd68f94
Brenna Pavlinchak <raven.mobile.design@gmail.com>
2022-10-07 22:41:38 -0400

    Setup of Lab3

---
+++ Lab3/Lab3/main.cpp
@@ -0,0 +1,51 @@
+//
+//  main.cpp
+//  Lab3
+//
+//  Created by Brenna Pavlinchak on 10/7/22.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int ArrayOfInts();
+int Pointer();
+int EnumColorDefinition();
+int ChangeColor();
+
+int main(int argc, const char * argv[])
+{
+    ArrayOfInts();
+    Pointer();
+    EnumColorDefinition();
+    ChangeColor();
+
+    return 0;
+}
+
+int ArrayOfInts()
+{
+    
+    return 0;
+}
+
+int Pointer()
+{
+    
+    return 0;
+}
+
+int EnumColorDefinition()
+{
+    
+    return 0;
+}
+
+int ChangeColor()
+{
+    
+    return 0;
+}
+
+
